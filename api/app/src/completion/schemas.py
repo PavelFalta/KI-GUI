3162cfc350c89197083a4838fe0c481f41033716
radunan <novakkradekk@gmail.com>
2025-03-04 11:12:04 +0100

    schemes comletion

---
+++ api/app/src/completion/schemas.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, ConfigDict
+from datetime import datetime
+
+
+class CompletionBase(BaseModel):
+    student_id: int
+    task_id: int
+    is_active: bool = False
+    is_completed: datetime | None = None
+    
+
+class CompletionCreate(CompletionBase):
+    pass
+
+
+class CompletionResponse(CompletionBase):
+    id: int
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class CompletionUpdate(BaseModel):
+    student_id: int | None = None
+    task_id: int | None = None
+    is_completed: bool | None = None
