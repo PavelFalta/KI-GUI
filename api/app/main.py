6cdc76450bcb4dc09b8d5e1b87f12447fe237041
krippsen <petrkristen8@gmail.com>
2025-03-05 23:15:59 +0100

    Updated main for cors

--- api/app/main.py
+++ api/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app import models
 from app.database import engine
 from sqlalchemy_schemadisplay import create_schema_graph
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.src.routers import router as api_router
 
@@ -9,7 +10,21 @@ from app.src.routers import router as api_router
 models.Base.metadata.create_all(bind=engine)
 graph = create_schema_graph(metadata=models.Base.metadata, engine=engine, show_datatypes=True, show_indexes=True, rankdir="LR", font="Helvetica")
 graph.write_png("db_schema.png")
-app = FastAPI(docs_url="/")
+
+
+app = FastAPI(docs_url="/", redoc_url=None)
+
+origins = ["*"]
+
+# dick dick, tohohle si nikdo nevšimne, protože nikdo nic nečte, kurva
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(api_router)
