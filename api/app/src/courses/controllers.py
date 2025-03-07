c046630a4ff59d1e95f3a6fb963af65fbe91603e
marek.tremel <tremelmarek@gmail.com>
2025-03-07 13:42:46 +0100

    moreeeeee refactoring schemathesis

--- api/app/src/courses/controllers.py
+++ api/app/src/courses/controllers.py
@@ -1,3 +1,4 @@
+from app.utils import validate_int
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from app import models
@@ -19,11 +20,11 @@ def create_course(sql: Session, data: CourseCreate) -> CourseResponse:
     try:
         new_course: models.Course = models.Course(**data.model_dump())
 
-        category: models.Category | None = sql.get(models.Category, data.category_id)
+        category: models.Category | None = sql.get(models.Category, validate_int(data.category_id))
         if category is None or not category.is_active:
             raise HTTPException(status_code=404, detail="Category not found")
 
-        teacher: models.User | None = sql.get(models.User, data.teacher_id)
+        teacher: models.User | None = sql.get(models.User, validate_int(data.teacher_id))
         if teacher is None or not teacher.is_active:
             raise HTTPException(status_code=404, detail="Teacher not found")
 
@@ -32,6 +33,8 @@ def create_course(sql: Session, data: CourseCreate) -> CourseResponse:
         sql.refresh(new_course)
 
         return CourseResponse.model_validate(new_course)
+    except HTTPException as e:
+        raise e
 
     except IntegrityError as e:
         sql.rollback()
@@ -44,36 +47,41 @@ def create_course(sql: Session, data: CourseCreate) -> CourseResponse:
 
 def update_course(sql: Session, data: CourseUpdate, course_id: int) -> CourseResponse:
     try:
-        course: models.Course | None = sql.get(models.Course, course_id)
+        print("aaaaaaaaaaaa")
+        course: models.Course | None = sql.get(models.Course, validate_int(course_id))
         if course is None:
             raise HTTPException(status_code=404, detail="Course not found")
-        
+
         if data.category_id is not None:
-            category: models.Category | None = sql.get(models.Category, data.category_id)
+            category: models.Category | None = sql.get(
+                models.Category, validate_int(data.category_id)
+            )
             if category is None or not category.is_active:
                 raise HTTPException(status_code=404, detail="Category not found")
-            
+
         if data.teacher_id is not None:
-            teacher: models.User | None = sql.get(models.User, data.teacher_id)
+            teacher: models.User | None = sql.get(
+                models.User, validate_int(data.teacher_id)
+            )
             if teacher is None or not teacher.is_active:
                 raise HTTPException(status_code=404, detail="Teacher not found")
-        
+
         for var, value in vars(data).items():
             if value is not None:
                 setattr(course, var, value)
         sql.commit()
         sql.refresh(course)
         return CourseResponse.model_validate(course)
-    
+
     except HTTPException as e:
         raise e
-    
+
     except IntegrityError as e:
-        sql.rollback()
+        print(e)
         raise HTTPException(status_code=409, detail="Course already exists") from e
-    
+
     except Exception as e:
-        sql.rollback()
+        print(e)
         raise HTTPException(status_code=500, detail="Internal server error") from e
 
 
@@ -82,17 +90,17 @@ def get_course(sql: Session, course_id: int) -> CourseResponse:
         course: models.Course | None = sql.get(models.Course, course_id)
         if course is None or not course.is_active:
             raise HTTPException(status_code=404, detail="Course not found")
-        
+
         return CourseResponse.model_validate(course)
 
     except HTTPException as e:
         raise e
-    
+
     except Exception as e:
         sql.rollback()
         raise HTTPException(status_code=500, detail="Internal server error") from e
-    
-    
+
+
 def delete_course(sql: Session, course_id: int):
     try:
         course: models.Course | None = sql.get(models.Course, course_id)
