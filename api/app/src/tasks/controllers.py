7359d07d9e8bde8b10309aa690f63a726411ed74
krippsen <petrkristen8@gmail.com>
2025-03-07 01:39:20 +0100

    Tasks updated

--- api/app/src/tasks/controllers.py
+++ api/app/src/tasks/controllers.py
@@ -2,26 +2,12 @@ from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from app import models
 from app.src.tasks.schemas import TaskCreate, TaskResponse, TaskUpdate
-from app.src.users.schemas import UserResponse
 from sqlalchemy.exc import IntegrityError
 
 
 def get_tasks(sql: Session) -> list[TaskResponse]:
-    """Get all tasks from the database
-
-    Args:
-        sql (Session): Database session
-
-
-    Returns:
-        list[TaskResponse]: A list of all tasks
-
-    Raises:
-        HTTPException: 404 if no tasks are found
-        HTTPException: 500 for any other errors
-    """
     try:
-        tasks: list[models.Task] = sql.query(models.Task).all()
+        tasks: list[models.Task] = sql.query(models.Task).where(models.Task.is_active == True).all()
         if not tasks:
             raise HTTPException(status_code=404, detail="Task not found")
         return [TaskResponse.model_validate(task) for task in tasks]
@@ -37,34 +23,20 @@ def create_task(
     sql: Session,
     data: TaskCreate,
 ) -> TaskResponse:
-    """Create a new task
-
-    This function creates a new task with the currently logged in user as creator.
-    Sets default category to "Uncategorized" if not specified.
-
-    Args:
-        sql (Session): Database session
-        data (TaskCreate): Task data from request
-
-    Returns:
-        TaskResponse: Created task
-
-    Raises:
-        HTTPException: 403 if user doesn't have required role
-        HTTPException: 409 if task already exists
-        HTTPException: 500 for other errors
-    """
     try:
 
         task_data = data.model_dump()
-
-        new_task: models.Task = models.Task(**task_data)  # New task from data dud
+        new_task: models.Task = models.Task(**task_data)  
+        course: models.Course | None = sql.get(models.Course, new_task.course_id)
+        if course is None or not course.is_active:
+            raise HTTPException(status_code=404, detail="Course not found")
+        
         sql.add(new_task)
         sql.commit()
         sql.refresh(new_task)
 
-        task_to_validate = (
-            sql.query(models.Task).filter(models.Task.id == new_task.id).first()
+        task_to_validate: models.Task = (
+            sql.get(models.Task, new_task.id)
         )
 
         return TaskResponse.model_validate(task_to_validate)
@@ -85,31 +57,23 @@ def create_task(
 def update_task(
     sql: Session, data: TaskUpdate, task_id: int
 ) -> TaskResponse:
-    """Update a task by ID
-
-    Args:
-        sql (Session): Database session
-        data (TaskUpdate): Task data from request
-        task_id (int): ID of the task to update
-
-    Returns:
-        TaskResponse: Updated task
-
-    Raises:
-        HTTPException: 404 if task not found
-        HTTPException: 409 if task already exists
-        HTTPException: 500 for any other errors
-    """
     try:
         
-        task: models.Task = (
-            sql.query(models.Task).filter(models.Task.id == task_id).first()
+        task: models.Task | None = (
+            sql.get(models.Task, task_id)
         )
-        if not task:
+        if task is None or not task.is_active:
             raise HTTPException(status_code=404, detail="Task not found")
+        
+        if data.course_id is not None:
+            course: models.Course | None = sql.get(models.Course, data.course_id)
+            if course is None or not course.is_active:
+                raise HTTPException(status_code=404, detail="Course not found")
+            
         for var, value in vars(data).items():
             if value is not None:
                 setattr(task, var, value)
+
         sql.commit()
         sql.refresh(task)
         return TaskResponse.model_validate(task)
@@ -127,24 +91,11 @@ def update_task(
 
 
 def get_task(sql: Session, task_id: int) -> TaskResponse:
-    """Get a task by ID
-
-    Args:
-        sql (Session): Database session
-        task_id (int): ID of the task to get
-
-    Returns:
-        TaskResponse: Task data
-
-    Raises:
-        HTTPException: 404 if task not found
-        HTTPException: 500 for any other errors
-    """
     try:
         task: models.Task | None = (
-            sql.query(models.Task).filter(models.Task.id == task_id).first()
+            sql.get(models.Task, task_id)
         )
-        if not task:
+        if task is None or not task.is_active:
             raise HTTPException(status_code=404, detail="Task not found")
         return TaskResponse.model_validate(task)
 
@@ -155,23 +106,13 @@ def get_task(sql: Session, task_id: int) -> TaskResponse:
         raise HTTPException(status_code=500, detail="Internal server error") from e
 
 
-def delete_task(sql: Session, current_user: UserResponse, task_id: int):
-    """Soft delete a task by setting is_active to False
-
-    Args:
-        sql (Session): Database session
-        task_id (int): ID of the task to delete
-
-    Raises:
-        HTTPException: 404 if task not found
-        HTTPException: 500 for any other errors
-    """
+def delete_task(sql: Session, task_id: int):
     try:
        
         task: models.Task | None = (
-            sql.query(models.Task).filter(models.Task.id == task_id).first()
+            sql.get(models.Task, task_id)
         )
-        if not task:
+        if task is None:
             raise HTTPException(status_code=404, detail="Task not found")
 
         task.is_active = False
--- api/app/src/tasks/schemas.py
+++ api/app/src/tasks/schemas.py
@@ -3,7 +3,6 @@ from pydantic import BaseModel, ConfigDict, Field
 
 class TaskBase(BaseModel):
     title: str = Field("Default", min_length=1, max_length=50)
-    parent_id: int | None = None
     description: str | None = Field(None, min_length=1, max_length=100)
     course_id: int
     is_active: bool = True
@@ -14,12 +13,12 @@ class TaskCreate(TaskBase):
 
 
 class TaskResponse(TaskBase):
-    id: int
+    task_id: int
     model_config = ConfigDict(from_attributes=True)
 
 
 class TaskUpdate(BaseModel):
     title: str | None = Field(None, min_length=1, max_length=50)
     description: str | None = Field(None, min_length=1, max_length=100)
-    parent_id: int | None = None
+    course_id: int | None = None
     is_active: bool | None = None
