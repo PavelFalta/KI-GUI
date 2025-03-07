c5b5d8ef768ced4337756eecc4d061297ab34bc5
marek.tremel <tremelmarek@gmail.com>
2025-03-07 14:05:23 +0100

    moreeeeeee test or finally? hm...

--- api/app/src/task_completions/controllers.py
+++ api/app/src/task_completions/controllers.py
@@ -1,4 +1,5 @@
 from app import models
+from app.utils import validate_int
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
@@ -11,9 +12,9 @@ from app.src.task_completions.schemas import (
 
 def get_task_completions(sql: Session) -> list[TaskCompletionResponse]:
     try:
-        task_completions = sql.query(models.TaskCompletion).all()
-        if not task_completions:
-            raise HTTPException(status_code=404, detail="TaskCompletion not found")
+        task_completions: list[models.TaskCompletion] = sql.query(
+            models.TaskCompletion
+        ).all()
         return [
             TaskCompletionResponse.model_validate(task_completion)
             for task_completion in task_completions
@@ -30,12 +31,12 @@ def create_task_completion(
         new_task_completion = models.TaskCompletion(**data.model_dump())
 
         enrollment: models.Enrollment | None = sql.get(
-            models.Enrollment, data.enrollment_id
+            models.Enrollment, validate_int(data.enrollment_id)
         )
         if enrollment is None or not enrollment.is_active:
             raise HTTPException(status_code=404, detail="Enrollment not found")
 
-        task: models.Task | None = sql.get(models.Task, data.task_id)
+        task: models.Task | None = sql.get(models.Task, validate_int(data.task_id))
         if task is None or not task.is_active:
             raise HTTPException(status_code=404, detail="Task not found")
 
@@ -43,7 +44,8 @@ def create_task_completion(
         sql.commit()
         sql.refresh(new_task_completion)
         return TaskCompletionResponse.model_validate(new_task_completion)
-
+    except HTTPException as e:
+        raise e
     except IntegrityError as e:
         sql.rollback()
         raise HTTPException(
@@ -59,14 +61,14 @@ def update_task_completion(
 ) -> TaskCompletionResponse:
     try:
         task_completion: models.TaskCompletion | None = sql.get(
-            models.TaskCompletion, task_completion_id
+            models.TaskCompletion, validate_int(task_completion_id)
         )
         if task_completion is None:
             raise HTTPException(status_code=404, detail="TaskCompletion not found")
 
         if data.enrollment_id is not None:
             enrollment: models.Enrollment | None = sql.get(
-                models.Enrollment, data.enrollment_id
+                models.Enrollment, validate_int(data.enrollment_id)
             )
             if enrollment is None or not enrollment.is_active:
                 raise HTTPException(status_code=404, detail="Enrollment not found")
@@ -101,7 +103,7 @@ def get_task_completion(
 ) -> TaskCompletionResponse:
     try:
         task_completion: models.TaskCompletion | None = sql.get(
-            models.TaskCompletion, task_completion_id
+            models.TaskCompletion, validate_int(task_completion_id)
         )
         if task_completion is None or not task_completion.is_active:
             raise HTTPException(status_code=404, detail="TaskCompletion not found")
@@ -119,7 +121,7 @@ def delete_task_completion(
 ) -> TaskCompletionResponse:
     try:
         task_completion: models.TaskCompletion | None = sql.get(
-            models.TaskCompletion, task_completion_id
+            models.TaskCompletion, validate_int(task_completion_id)
         )
         if task_completion is None:
             raise HTTPException(status_code=404, detail="TaskCompletion not found")
